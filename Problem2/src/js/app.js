37767bd5affdabffe7253a47748754be61107512
Moeen Mahmud <write2fahim007@gmail.com>
2022-04-21 04:20:22 +0600

    added fetch system for generating people

--- Problem2/src/js/app.js
+++ Problem2/src/js/app.js
@@ -1,3 +1,17 @@
 // Random Number Function
 const randomizeNumber = (minNumber, maxNumber) =>
   parseInt(Math.random() * (maxNumber - minNumber)) + minNumber;
+
+const endpoint = "https://swapi.dev/api/people/";
+
+const fetchPeople = async () => {
+  try {
+    const response = await fetch(`${endpoint}${randomizeNumber(1, 11)}`);
+    const data = await response.json();
+    console.log(data);
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+fetchPeople();
