47465b5cecc7ee744ee5a94ebb894f607224e940
Moeen Mahmud <write2fahim007@gmail.com>
2022-04-21 05:10:01 +0600

    added add button functionality

--- Problem2/src/js/app.js
+++ Problem2/src/js/app.js
@@ -3,15 +3,44 @@ const randomizeNumber = (minNumber, maxNumber) =>
   parseInt(Math.random() * (maxNumber - minNumber)) + minNumber;
 
 const endpoint = "https://swapi.dev/api/people/";
+const dataTable = document.getElementById("data-table");
+const addButton = document.getElementById("add-button");
 
 const fetchPeople = async () => {
+  addButton.setAttribute("disabled", true);
+  let generatedPeople = [];
   try {
     const response = await fetch(`${endpoint}${randomizeNumber(1, 11)}`);
     const data = await response.json();
-    console.log(data);
+    generatedPeople.push(data);
+    displayPeople(generatedPeople);
+    console.log(generatedPeople);
+    addButton.removeAttribute("disabled");
   } catch (err) {
     console.log(err.message);
   }
 };
 
-fetchPeople();
+const displayPeople = (peopleArray) => {
+  if (peopleArray?.length !== 0) {
+    peopleArray?.forEach((people) => {
+      const id = people?.homeworld;
+      const peopleName = people?.name;
+
+      const tableBody = document.createElement("tr");
+
+      tableBody.innerHTML = `
+          <td>${peopleName}</td>
+          <td>
+            <button onclick="showFilteredPost('${id}')" 
+              type="button"
+            >
+              Delete
+            </button>
+          </td>
+      `;
+
+      dataTable.appendChild(tableBody);
+    });
+  }
+};
