c30bb31c9ef72164598180908146231edb5dabc9
Moeen Mahmud <write2fahim007@gmail.com>
2022-04-21 07:43:09 +0600

    refactored the code

--- Problem2/src/js/app.js
+++ Problem2/src/js/app.js
@@ -1,26 +1,57 @@
-// Random Number Function
+/**
+ * Function that generates random number
+ * @param {integer} minNumber
+ * @param {integer} maxNumber
+ * @returns integer
+ */
 const randomizeNumber = (minNumber, maxNumber) =>
   parseInt(Math.random() * (maxNumber - minNumber)) + minNumber;
 
+// Globals
 const endpoint = "https://swapi.dev/api/people/";
 const dataTable = document.getElementById("data-table");
 const addButton = document.getElementById("add-button");
 
+/**
+ * Function for deleting
+ * @param {string} id
+ */
+const deletePeople = (id) => {
+  return document.getElementById(id).remove();
+};
+
+/**
+ * Function for fetching the people
+ */
 const fetchPeople = async () => {
+  // Disabling the Add Button on each click to avoid overload fetching
+  // or preventing repeatative clicking.
   addButton.setAttribute("disabled", true);
+
+  // empty array for storing the peoples
   let generatedPeople = [];
   try {
     const response = await fetch(`${endpoint}${randomizeNumber(1, 11)}`);
     const data = await response.json();
+
+    // Populated the above empty array
     generatedPeople.push(data);
+
+    // send the data to that function
     displayPeople(generatedPeople);
-    console.log(generatedPeople);
-    addButton.removeAttribute("disabled");
   } catch (err) {
     console.log(err.message);
+  } finally {
+    // Finally removed the disabled attribute
+    addButton.removeAttribute("disabled");
   }
 };
 
+/**
+ * Function for looping through the peoples data and render them
+ * in the table body
+ * @param {array} peopleArray
+ */
 const displayPeople = (peopleArray) => {
   if (peopleArray?.length !== 0) {
     peopleArray?.forEach((people) => {
@@ -28,12 +59,16 @@ const displayPeople = (peopleArray) => {
       const peopleName = people?.name;
 
       const tableBody = document.createElement("tr");
+
+      // Setting up the element with a unique id for deleting
       tableBody.setAttribute("id", id + peopleName);
 
       tableBody.innerHTML = `
           <td>${peopleName}</td>
           <td>
-            <button onclick="deletePeople('${id + peopleName}')" 
+            <button class="delete-button" onclick="deletePeople('${
+              id + peopleName
+            }')" 
               type="button"
             >
               Delete
@@ -44,8 +79,4 @@ const displayPeople = (peopleArray) => {
       dataTable.appendChild(tableBody);
     });
   }
-};
-
-const deletePeople = (id) => {
-  return document.getElementById(id).remove();
 };
